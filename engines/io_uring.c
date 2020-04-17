0588820445b2a2ac90a9cebf60d130ecb56aa396
Stefano Garzarella <sgarzare@redhat.com>
2020-04-17 15:54:17 +0200

    iouring: add initial passthrough implementation

--- engines/io_uring.c
+++ engines/io_uring.c
@@ -18,6 +18,8 @@
 #include "../lib/memalign.h"
 #include "../lib/fls.h"
 
+#define IO_URING_PT
+
 #ifdef ARCH_HAVE_IOURING
 
 #include "../lib/types.h"
@@ -45,6 +47,70 @@ struct ioring_mmap {
 	size_t len;
 };
 
+#ifdef IO_URING_PT
+
+#define build_mmio_read(name, size, type, reg, barrier) \
+static inline type name(const volatile void  *addr) \
+{ type ret; asm volatile("mov" size " %1,%0":reg (ret) \
+:"m" (*(volatile type *)addr) barrier); return ret; }
+
+#define build_mmio_write(name, size, type, reg, barrier) \
+static inline void name(type val, volatile void  *addr) \
+{ asm volatile("mov" size " %0,%1": :reg (val), \
+"m" (*(volatile type *)addr) barrier); }
+
+build_mmio_read(readb, "b", unsigned char, "=q", :"memory")
+build_mmio_read(readw, "w", unsigned short, "=r", :"memory")
+build_mmio_read(readl, "l", unsigned int, "=r", :"memory")
+
+build_mmio_write(writeb, "b", unsigned char, "q", :"memory")
+build_mmio_write(writew, "w", unsigned short, "r", :"memory")
+build_mmio_write(writel, "l", unsigned int, "r", :"memory")
+
+static inline void iowrite32(uint32_t value, volatile void *addr)
+{
+	writel(value, addr);
+}
+
+static inline uint32_t ioread32(const volatile void *addr)
+{
+	return readl(addr);
+}
+
+
+#define IO_URING_MR_SIZE (1 << 15)
+
+struct virtio_blk_iouring_enter {
+	unsigned int to_submit;
+	unsigned int min_complete;
+	unsigned int flags;
+};
+
+struct virtio_blk_iouring {
+	uint64_t mr_base;
+	uint64_t mr_size;
+	uint64_t phy_offset;
+	uint64_t sqcq_offset;
+	uint64_t sqes_offset;
+	uint64_t kick_offset;
+	uint64_t userspace;
+	struct io_uring_params params;
+	struct virtio_blk_iouring_enter enter;
+};
+
+struct ioring_pt_io_phys {
+	uint64_t iov_phy;
+	uint64_t buf_phy;
+};
+
+struct ioring_pt {
+	struct ioring_pt_io_phys *io_phys;
+	struct virtio_blk_iouring *vbi;
+	void *kick_addr;
+	uint64_t phy_offset;
+};
+#endif
+
 struct ioring_data {
 	int ring_fd;
 
@@ -55,6 +121,9 @@ struct ioring_data {
 	struct io_sq_ring sq_ring;
 	struct io_uring_sqe *sqes;
 	struct iovec *iovecs;
+#ifdef IO_URING_PT
+	struct ioring_pt iou_pt;
+#endif
 	unsigned sq_ring_mask;
 
 	struct io_cq_ring cq_ring;
@@ -80,6 +149,9 @@ struct ioring_options {
 	unsigned int sqpoll_cpu;
 	unsigned int nonvectored;
 	unsigned int uncached;
+#ifdef IO_URING_PT
+	uint64_t ioupt_base_address;
+#endif /* IO_URING_PT */
 };
 
 static const int ddir_to_op[2][2] = {
@@ -185,14 +257,87 @@ static struct fio_option options[] = {
 		.category = FIO_OPT_C_ENGINE,
 		.group	= FIO_OPT_G_IOURING,
 	},
+#ifdef IO_URING_PT
+	{
+		.name	= "io_uring_pt",
+		.lname	= "io_uring pt",
+		.type	= FIO_OPT_ULL,
+		.off1	= offsetof(struct ioring_options, ioupt_base_address),
+		.help	= "io_uring passthrough base address",
+		.category = FIO_OPT_C_ENGINE,
+		.group	= FIO_OPT_G_IOURING,
+	},
+#endif
 	{
 		.name	= NULL,
 	},
 };
 
-static int io_uring_enter(struct ioring_data *ld, unsigned int to_submit,
+#ifdef IO_URING_PT
+#include <linux/types.h>
+/*
+ * Get physical address of any mapped virtual address in the current process.
+ */
+uint64_t
+virt2phy(struct thread_data *td, const void *virtaddr)
+{
+	int fd;
+	uint64_t page, physaddr;
+	unsigned long virt_pfn;
+	int page_size;
+	off_t offset;
+
+	/* standard page size */
+	page_size = getpagesize();
+
+	fd = open("/proc/self/pagemap", O_RDONLY);
+	if (fd < 0) {
+		td_verror(td, errno, "open /proc/self/pagemap ");
+		return 0;
+	}
+
+	virt_pfn = (unsigned long)virtaddr / page_size;
+	offset = sizeof(uint64_t) * virt_pfn;
+	if (lseek(fd, offset, SEEK_SET) == (off_t) -1) {
+		td_verror(td, errno, "lseek /proc/self/pagemap ");
+		return 0;
+	}
+	if (read(fd, &page, sizeof(uint64_t)) < 0) {
+		td_verror(td, errno, "open /proc/self/pagemap ");
+		return 0;
+	}
+
+	/*
+	 * the pfn (page frame number) are bits 0-54 (see
+	 * pagemap.txt in linux Documentation)
+	 */
+	physaddr = ((page & 0x7fffffffffffffULL) * page_size)
+		+ ((unsigned long)virtaddr % page_size);
+	close(fd);
+
+	return physaddr;
+}
+#endif
+
+static int io_uring_enter(struct ioring_data *ld, struct ioring_options *o,
+			 unsigned int to_submit,
 			 unsigned int min_complete, unsigned int flags)
 {
+
+#ifdef IO_URING_PT
+	if (o->ioupt_base_address) {
+		printf("kick - submitted %u wait_nr %u flags %u\n",
+		       to_submit, min_complete, flags);
+
+		ld->iou_pt.vbi->enter.to_submit = to_submit;
+		ld->iou_pt.vbi->enter.min_complete = min_complete;
+		ld->iou_pt.vbi->enter.flags = flags;
+
+		iowrite32(1, ld->iou_pt.kick_addr);
+
+		return ioread32(ld->iou_pt.kick_addr + 4);
+	}
+#endif
 	return syscall(__NR_io_uring_enter, ld->ring_fd, to_submit,
 			min_complete, flags, NULL, 0);
 }
@@ -209,6 +354,12 @@ static int fio_ioring_prep(struct thread_data *td, struct io_u *io_u)
 	/* zero out fields not used in this submission */
 	memset(sqe, 0, sizeof(*sqe));
 
+#ifdef IO_URING_PT
+	if (o->ioupt_base_address) {
+		sqe->fd = 0;
+		sqe->flags = IOSQE_FIXED_FILE;
+	} else
+#endif
 	if (o->registerfiles) {
 		sqe->fd = f->engine_pos;
 		sqe->flags = IOSQE_FIXED_FILE;
@@ -219,16 +370,34 @@ static int fio_ioring_prep(struct thread_data *td, struct io_u *io_u)
 	if (io_u->ddir == DDIR_READ || io_u->ddir == DDIR_WRITE) {
 		if (o->fixedbufs) {
 			sqe->opcode = fixed_ddir_to_op[io_u->ddir];
+#ifdef IO_URING_PT
+			if (o->ioupt_base_address) {
+				sqe->addr = ld->iou_pt.io_phys[io_u->index].buf_phy +
+						io_u->offset;
+			} else
+#endif
 			sqe->addr = (unsigned long) io_u->xfer_buf;
 			sqe->len = io_u->xfer_buflen;
 			sqe->buf_index = io_u->index;
 		} else {
 			sqe->opcode = ddir_to_op[io_u->ddir][!!o->nonvectored];
 			if (o->nonvectored) {
+#ifdef IO_URING_PT
+				if (o->ioupt_base_address) {
+					sqe->addr =
+					    ld->iou_pt.io_phys[io_u->index].buf_phy;
+				} else
+#endif
 				sqe->addr = (unsigned long)
 						ld->iovecs[io_u->index].iov_base;
 				sqe->len = ld->iovecs[io_u->index].iov_len;
 			} else {
+#ifdef IO_URING_PT
+				if (o->ioupt_base_address) {
+					sqe->addr =
+					    ld->iou_pt.io_phys[io_u->index].iov_phy;
+				} else
+#endif
 				sqe->addr = (unsigned long) &ld->iovecs[io_u->index];
 				sqe->len = 1;
 			}
@@ -322,7 +491,7 @@ static int fio_ioring_getevents(struct thread_data *td, unsigned int min,
 		}
 
 		if (!o->sqpoll_thread) {
-			r = io_uring_enter(ld, 0, actual_min,
+			r = io_uring_enter(ld, o, 0, actual_min,
 						IORING_ENTER_GETEVENTS);
 			if (r < 0) {
 				if (errno == EAGAIN || errno == EINTR)
@@ -427,7 +596,7 @@ static int fio_ioring_commit(struct thread_data *td)
 
 		read_barrier();
 		if (*ring->flags & IORING_SQ_NEED_WAKEUP)
-			io_uring_enter(ld, ld->queued, 0,
+			io_uring_enter(ld, o, ld->queued, 0,
 					IORING_ENTER_SQ_WAKEUP);
 		ld->queued = 0;
 		return 0;
@@ -437,7 +606,7 @@ static int fio_ioring_commit(struct thread_data *td)
 		unsigned start = *ld->sq_ring.head;
 		long nr = ld->queued;
 
-		ret = io_uring_enter(ld, nr, 0, IORING_ENTER_GETEVENTS);
+		ret = io_uring_enter(ld, o, nr, 0, IORING_ENTER_GETEVENTS);
 		if (ret > 0) {
 			fio_ioring_queued(td, start, ret);
 			io_u_mark_submit(td, ret);
@@ -483,6 +652,12 @@ static void fio_ioring_cleanup(struct thread_data *td)
 
 		free(ld->io_u_index);
 		free(ld->iovecs);
+#ifdef IO_URING_PT
+		free(ld->iou_pt.io_phys);
+		struct ioring_options *o = td->eo;
+		if (o->ioupt_base_address)
+			ld->iou_pt.vbi->userspace = 0;
+#endif
 		free(ld->fds);
 		free(ld);
 	}
@@ -528,6 +703,42 @@ static int fio_ioring_mmap(struct ioring_data *ld, struct io_uring_params *p)
 	return 0;
 }
 
+#ifdef IO_URING_PT
+static int fio_ioring_pt_mmap(struct ioring_data *ld, struct io_uring_params *p,
+			      void *sqcq, void *sqes)
+{
+	struct io_sq_ring *sring = &ld->sq_ring;
+	struct io_cq_ring *cring = &ld->cq_ring;
+	void *ptr;
+
+	if (!(p->features & IORING_FEAT_SINGLE_MMAP)) {
+		printf("IORING_FEAT_SINGLE_MMAP needed!");
+		errno = EINVAL;
+		return -errno;
+	}
+
+	ptr = sqcq;
+
+	sring->head = ptr + p->sq_off.head;
+	sring->tail = ptr + p->sq_off.tail;
+	sring->ring_mask = ptr + p->sq_off.ring_mask;
+	sring->ring_entries = ptr + p->sq_off.ring_entries;
+	sring->flags = ptr + p->sq_off.flags;
+	sring->array = ptr + p->sq_off.array;
+	ld->sq_ring_mask = *sring->ring_mask;
+
+	ld->sqes = sqes;
+
+	cring->head = ptr + p->cq_off.head;
+	cring->tail = ptr + p->cq_off.tail;
+	cring->ring_mask = ptr + p->cq_off.ring_mask;
+	cring->ring_entries = ptr + p->cq_off.ring_entries;
+	cring->cqes = ptr + p->cq_off.cqes;
+	ld->cq_ring_mask = *cring->ring_mask;
+	return 0;
+}
+#endif
+
 static int fio_ioring_queue_init(struct thread_data *td)
 {
 	struct ioring_data *ld = td->io_ops_data;
@@ -535,6 +746,43 @@ static int fio_ioring_queue_init(struct thread_data *td)
 	int depth = td->o.iodepth;
 	struct io_uring_params p;
 	int ret;
+
+#ifdef IO_URING_PT
+	if (o->ioupt_base_address) {
+		struct virtio_blk_iouring *vbi;
+		void *mr_base;
+		int mem_fd;
+
+		mem_fd = open("/dev/mem", O_RDWR | O_SYNC);
+		if (mem_fd < 0)
+			return mem_fd;
+
+		mr_base = mmap(NULL, IO_URING_MR_SIZE, PROT_READ | PROT_WRITE,
+			       MAP_SHARED | MAP_LOCKED, mem_fd,
+			       o->ioupt_base_address);
+		if (mr_base == NULL || mr_base == MAP_FAILED)
+			return -errno;
+
+		vbi = mr_base;
+		ld->iou_pt.vbi = vbi;
+		ld->iou_pt.phy_offset = vbi->phy_offset;
+		ld->iou_pt.kick_addr = mr_base + vbi->kick_offset;
+
+		printf("sqes val(uint64_t): %ld\n",
+		       *((uint64_t *)(mr_base + vbi->sqes_offset)));
+		printf("sqcq val(uint64_t): %ld\n",
+		       *((uint64_t *)(mr_base + vbi->sqcq_offset)));
+
+		printf("mr_base %p kick_offset %lu kick_addr %p\n",
+		       mr_base, vbi->kick_offset, ld->iou_pt.kick_addr);
+
+		ld->iou_pt.vbi->userspace = 1;
+
+		return fio_ioring_pt_mmap(ld, &vbi->params,
+					  mr_base + vbi->sqcq_offset,
+					  mr_base + vbi->sqes_offset);
+	}
+#endif
 
 	memset(&p, 0, sizeof(p));
 
@@ -579,6 +827,11 @@ static int fio_ioring_register_files(struct thread_data *td)
 	unsigned int i;
 	int ret;
 
+#ifdef IO_URING_PT
+	struct ioring_options *o = td->eo;
+	if (o->ioupt_base_address)
+		return 0;
+#endif
 	ld->fds = calloc(td->o.nr_files, sizeof(int));
 
 	for_each_file(td, f, i) {
@@ -633,6 +886,25 @@ static int fio_ioring_post_init(struct thread_data *td)
 		return 1;
 	}
 
+#ifdef IO_URING_PT
+	if (o->ioupt_base_address) {
+		for (i = 0; i < td->o.iodepth; i++) {
+			struct iovec *iov = &ld->iovecs[i];
+			io_u = ld->io_u_index[i];
+
+			if (mlock(iov->iov_base, iov->iov_len)) {
+				td_verror(td, errno, "mlock");
+				return 1;
+			}
+
+			ld->iou_pt.io_phys[i].buf_phy = ld->iou_pt.phy_offset +
+							virt2phy(td, io_u->buf);
+			ld->iou_pt.io_phys[i].iov_phy = ld->iou_pt.phy_offset +
+							virt2phy(td, iov);
+			iov->iov_base = (void *)ld->iou_pt.io_phys[i].buf_phy;
+		}
+	}
+#endif
 	if (o->registerfiles) {
 		err = fio_ioring_register_files(td);
 		if (err) {
@@ -655,6 +927,11 @@ static int fio_ioring_init(struct thread_data *td)
 	struct ioring_data *ld;
 	struct thread_options *to = &td->o;
 
+#ifdef IO_URING_PT
+	if (o->ioupt_base_address)
+		o->sqpoll_thread = 1;
+#endif
+
 	/* sqthread submission requires registered files */
 	if (o->sqpoll_thread)
 		o->registerfiles = 1;
@@ -675,6 +952,14 @@ static int fio_ioring_init(struct thread_data *td)
 	ld->io_u_index = calloc(td->o.iodepth, sizeof(struct io_u *));
 	ld->iovecs = calloc(td->o.iodepth, sizeof(struct iovec));
 
+#ifdef IO_URING_PT
+	ld->iou_pt.io_phys = calloc(td->o.iodepth, sizeof(*ld->iou_pt.io_phys));
+	if (o->ioupt_base_address &&
+		mlock(ld->iovecs, td->o.iodepth * sizeof(struct iovec))) {
+		td_verror(td, errno, "mlock");
+		return 1;
+	}
+#endif
 	td->io_ops_data = ld;
 
 	/*
@@ -712,6 +997,10 @@ static int fio_ioring_open_file(struct thread_data *td, struct fio_file *f)
 	if (!ld || !o->registerfiles)
 		return generic_open_file(td, f);
 
+#ifdef IO_URING_PT
+	if (o->ioupt_base_address)
+		return 0;
+#endif
 	f->fd = ld->fds[f->engine_pos];
 	return 0;
 }
